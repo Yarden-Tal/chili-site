528ded18293ee390d6fc8a4b18b16b6c079fdbff
Yarden Tal <yarden.tal@quatro-digital.com>
2023-04-22 13:29:11 +0300

    first commit

---
+++ pepper-store/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <Peppers />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+    import Peppers from '@/components/Peppers.vue';
+</script>
---
+++ pepper-store/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <Peppers />
+</template>
+
+<script lang="ts" setup>
+  import Peppers from '@/components/Peppers.vue';
+</script>
